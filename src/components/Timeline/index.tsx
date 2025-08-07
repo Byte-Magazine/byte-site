cdd39fcd1fa0bfba10ede675eaf530fbf7a3c5c3
Moeein Aali <moeeeinaali@gmail.com>
2025-08-07 23:25:35 +0330

    [FEAT] add timeline

---
+++ src/components/Timeline/index.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styles from "./styles.module.css";
+import shared from "../HomepageFeatures/styles.module.css";
+import {featureList} from "@site/src/data/FEATURE_LIST";
+
+const Timeline: React.FC = () => {
+        return (
+            <div className={styles.timelineContainer}>
+                <div className={styles.timelineLine}/>
+                {featureList.map((feature, index) => (
+                    <a href={`/mags/${feature.title}`} key={index} style={{textDecoration: "none"}}
+                       className={styles.timelineItem}>
+                        <div className={styles.timelineDot}
+                             style={{
+                                 "--card-hover-color": feature.themeColor,
+                             } as React.CSSProperties}
+                        >
+                            <img src="/img/logo.svg" alt="Byte Logo"/>
+                        </div>
+                        <div
+                            className={shared.card}
+
+                            style={{
+                                "--card-hover-color": feature.themeColor,
+                                paddingBottom: "20px"
+                            } as React.CSSProperties}
+                        >
+                            <div className={shared.title}>{feature.title}</div>
+                            <div className={styles.date}>{feature.date}</div>
+                            <p className={"font-snapp"}>{feature.description}</p>
+                            <a
+                                href={`https://s3.byte-mag.ir/mags/${feature.title}.pdf`}
+                                className={`${shared.btn} ${shared.primaryBtn}`}
+                                style={{padding: "0.2rem 0.6rem", textDecoration: "none"}}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                download
+                            >
+                                دانلود فایل نشریه
+                            </a>
+                        </div>
+                    </a>
+                ))}
+            </div>
+        )
+            ;
+    }
+;
+
+export default Timeline;
--- src/data/FEATURE_LIST.ts
+++ src/data/FEATURE_LIST.ts
@@ -1,20 +1,23 @@
 export const featureList = [
     {
         title: "00000001",
+        date: "۱۴۰۲/۰۹/۰۷",
         imageSrc: "/img/00000001.jpg",
-        description: "اولین شماره نشریه‌ی بایت",
+        description: "اولین شماره‌ی بایت",
         themeColor: "rgba(73,94,113,0.3)",
     },
     {
         title: "00000010",
+        date: "۱۴۰۴/۰۲/۳۱",
         imageSrc: "/img/00000010.jpg",
-        description: "دومین شماره نشریه‌ی بایت",
+        description: "دومین شماره‌ی بایت",
         themeColor: "rgba(97,136,241,0.5)",
     },
     {
         title: "00000011",
+        date: "۱۴۰۴/۰۴/۲۲",
         imageSrc: "/img/00000011.jpg",
-        description: "سومین شماره نشریه‌ی بایت",
+        description: "سومین شماره‌ی بایت",
         themeColor: "rgba(210,4,4,0.3)",
     }
 ];
