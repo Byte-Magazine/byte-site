a7519f6a310120245aff4356d4dcb12a38786b21
Moeein Aali <moeeeinaali@gmail.com>
2025-08-10 12:27:11 +0330

    [FIX] show tooltip with hover in desktop

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,17 +1,50 @@
-import {ReactElement, useState} from 'react';
+import {ReactElement, useState, useEffect, useRef} from 'react';
 import styles from './styles.module.css';
+import clsx from "clsx";
 
 export default function Tooltip({children, tip}): ReactElement {
     const [isVisible, setIsVisible] = useState(false);
+    const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
-    const toggleTooltip = () => {
-        setIsVisible(prev => !prev);
-    };
+    useEffect(() => {
+        return () => {
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
+            }
+        };
+    }, []);
 
     return (
-        <span className={styles.wrapper} onClick={toggleTooltip}>
+        <span
+            className={styles.wrapper}
+            onClick={() => {
+                setIsVisible(true);
+                if (timeoutRef.current) {
+                    clearTimeout(timeoutRef.current);
+                }
+                timeoutRef.current = setTimeout(() => {
+                    setIsVisible(false);
+                }, 3000);
+            }}
+            onMouseEnter={() => {
+                setIsVisible(true);
+            }}
+            onMouseLeave={() => {
+                setIsVisible(false);
+            }}
+        >
             <span className={styles.text}>{children}</span>
-            {isVisible && <span className={styles.tooltip}>{tip}</span>}
+            <span
+                className={clsx(
+                    styles.tooltip,
+                    {
+                        [styles.show]: isVisible,
+                        [styles.hide]: !isVisible
+                    }
+                )}
+            >
+                {tip}
+            </span>
         </span>
     );
 }
