a0b220b51d2a2ac730323f8a70c60acea49dea5a
Moeein Aali <moeeeinaali@gmail.com>
2025-11-11 09:56:50 +0330

    [ADD] codenameh

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -2,64 +2,71 @@ import React from "react";
 import Link from "@docusaurus/Link";
 
 import styles from "./styles.module.css";
-import { featureList } from "@site/src/data/FEATURE_LIST";
+import {featureList} from "@site/src/data/FEATURE_LIST";
 
-function Feature({ title, imageSrc, description, themeColor }) {
-  return (
-    <div
-      className={`col col--3 ${styles.cardWrapper}`}
-      onClick={() => (window.location.href = `/mags/${title}`)}
-      style={{ cursor: "pointer" }}
-    >
-      <div
-        className={styles.card}
-        style={{ "--card-hover-color": themeColor } as React.CSSProperties}
-      >
-        <div className={styles.imageWrapper}>
-          <img className={styles.poster} src={imageSrc} alt={title} />
-          <div
-            className={styles.issueTitle}
-            style={{ backgroundColor: themeColor }}
-          >
-            {title}
-          </div>
+export function Feature({title, imageSrc, description, themeColor, slug = "", file = ""}) {
+    return (
+        <div
+            className={`col col--3 ${styles.cardWrapper}`}
+            onClick={() => (window.location.href = `/mags/${title}`)}
+            style={{cursor: "pointer"}}
+        >
+            <div
+                className={styles.card}
+                style={{"--card-hover-color": themeColor} as React.CSSProperties}
+            >
+                <div className={styles.imageWrapper}>
+                    <img className={styles.poster} src={imageSrc} alt={title}/>
+                    <div
+                        className={styles.issueTitle}
+                        style={{backgroundColor: themeColor}}
+                    >
+                        {title}
+                    </div>
+                </div>
+
+                <h3 className={styles.title}>{description}</h3>
+
+                <div className={styles.buttons} onClick={(e) => e.stopPropagation()}>
+                    {
+                        file && (
+                            <a
+                                href={`https://s3.byte-mag.ir/mags/${file}.pdf`}
+                                className={`${styles.btn} ${styles.secondaryBtn}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                download
+                            >
+                                دانلود نشریه
+                            </a>
+                        )
+                    }
+                    {
+                        slug && (
+                            <Link
+                                to={`/mags/${slug}`}
+                                className={`${styles.btn} ${styles.primaryBtn}`}
+                            >
+                                مشاهده متن‌ها
+                            </Link>
+                        )
+                    }
+                </div>
+            </div>
         </div>
-
-        <h3 className={styles.title}>{description}</h3>
-
-        <div className={styles.buttons} onClick={(e) => e.stopPropagation()}>
-          <a
-            href={`https://s3.byte-mag.ir/mags/${title}.pdf`}
-            className={`${styles.btn} ${styles.secondaryBtn}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            download
-          >
-            دانلود نشریه
-          </a>
-
-          <Link
-            to={`/mags/${title}`}
-            className={`${styles.btn} ${styles.primaryBtn}`}
-          >
-            مشاهده متن‌ها
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default function HomepageFeatures() {
-  return (
-    <section className="padding-vert--lg">
-      <div className="container">
-        <div className={styles.centerRow}>
-          {featureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className="padding-vert--lg">
+            <div className="container">
+                <div className={styles.centerRow}>
+                    {featureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
